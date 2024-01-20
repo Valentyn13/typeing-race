8e433aa7b6f9d1215819d9e0a887aed5fe0af995
Valentyn13 <valentyn13git@gmail.com>
2024-01-20 08:02:13 +0200

    typing-race: * data.ts file -add app data

--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,5 @@
+import { IClientList, IRoomList, IRoomMap } from "./types/client.types.js";
+
 export const texts = [
     'My first super text!',
     'My second super text!',
@@ -7,5 +9,11 @@ export const texts = [
     'My sixth super text!',
     'My seventh super text!'
 ];
+
+
+export const clients: IClientList = []
+
+
+export const roomsMapp:IRoomMap = new Map()
 
 export default { texts };
