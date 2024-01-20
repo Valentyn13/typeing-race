f2e7ae11e47c2a56642a3a4bd349e51d29b50f08
Valentyn13 <valentyn13git@gmail.com>
2024-01-20 08:05:59 +0200

    typing-race: * server.ts -add simple user check

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { Server } from 'socket.io';
 import socketHandler from './socket/socket.js';
 import routes from './routes/routes.js';
 import { STATIC_PATH, PORT } from './config.js';
+import { clients } from './data.js';
 
 const app = express();
 const httpServer = new http.Server(app);
@@ -15,6 +16,30 @@ app.use(express.static(STATIC_PATH));
 routes(app);
 
 app.get('*', (req, res) => res.redirect('/login'));
+
+socketIo.use((socket,next) => {
+    const username = socket.handshake.query.username as string
+    if (!username) {
+        next(new Error(`Username must be provided!`))
+    }
+
+
+    socket.on('disconnect',() => {
+        const index = clients.findIndex(client => client.username === username)
+        clients.splice(index,1)
+    })
+
+    const isClientWithNameExist = clients.find(client => client.username === username)
+
+    if (isClientWithNameExist){
+        next(new Error(`User with name ${username} already connected`))
+    } else {
+        clients.push({id:socket.id, username})
+        console.log(clients)
+        next()
+    }
+})
+
 
 socketHandler(socketIo);
 
